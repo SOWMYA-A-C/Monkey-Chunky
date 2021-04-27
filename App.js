0532e7f5de4eadf579c4631aab6c407146b1f004
SOWMYA-A-C <ssb.soumya@gmail.com>
2021-04-28 00:03:15 +0530

    changes done

--- App.js
+++ App.js
@@ -81,10 +81,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#b8b8b8',
   },
   inputBox: {
-    marginTop: 50,
+    marginTop: 40,
     width: '80%',
     alignSelf: 'center',
-    height: 40,
+    height: 38,
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
